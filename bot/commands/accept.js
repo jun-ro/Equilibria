bf89e4c5bcf6e17483ce603041a01eda5d401a05
jun 3 <97373683+jun-ro@users.noreply.github.com>
2023-04-04 00:00:57 -0400

    Update accept.js

--- bot/commands/accept.js
+++ bot/commands/accept.js
@@ -27,7 +27,7 @@ module.exports = {
     const cat = await randomCat();
     const catPfp = cat.data.message;
     try {
-      await fetch(`http://localhost:3000/acceptForum`, {
+      await fetch(`https://odd-blue-mackerel-coat.cyclic.app/acceptForum`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
